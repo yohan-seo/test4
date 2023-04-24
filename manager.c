dc64c643fa17a56a7455a602cf0ff74718d13561
Your Name <you@example.com>
2023-04-24 18:25:25 +0900

    add save

--- manager.c
+++ manager.c
@@ -40,7 +40,9 @@ void saveData(Product p[], int count){
 
 	//중량 가격 제품명
 	fp= fopen("product.txt","wt");
-	
+	for(int i=0;i<count;i++){
+	fprintf(fp, "%s %d %d\n", p[i].name, p[i].weight, p[i].price);
+	}	
 	
 	fclose(fp);
 	printf("저장됨!\n");
